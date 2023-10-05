a82c8986e89d33ae734b6beac1d4303901805edd
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-10-05 16:42:49 +0300

    feat: implement `NavigationContainer` and set it up

--- App.js
+++ App.js
@@ -1,53 +1,24 @@
 import React from "react";
-import { SafeAreaView, View, Text, StyleSheet, FlatList } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import "react-native-gesture-handler";
 
-import ColorBox from "./components/ColorBox";
+import Home from "./screens/Home";
+import ColorPalette from "./screens/ColorPalette";
 
-const COLORS = [
-  { colorName: "Base03", hexCode: "#002b36" },
-  { colorName: "Base02", hexCode: "#073642" },
-  { colorName: "Base01", hexCode: "#586e75" },
-  { colorName: "Base00", hexCode: "#657b83" },
-  { colorName: "Base0", hexCode: "#839496" },
-  { colorName: "Base1", hexCode: "#93a1a1" },
-  { colorName: "Base2", hexCode: "#eee8d5" },
-  { colorName: "Base3", hexCode: "#fdf6e3" },
-  { colorName: "Yellow", hexCode: "#b58900" },
-  { colorName: "Orange", hexCode: "#cb4b16" },
-  { colorName: "Red", hexCode: "#dc322f" },
-  { colorName: "Magenta", hexCode: "#d33682" },
-  { colorName: "Violet", hexCode: "#6c71c4" },
-  { colorName: "Blue", hexCode: "#268bd2" },
-  { colorName: "Cyan", hexCode: "#2aa198" },
-  { colorName: "Green", hexCode: "#859900" },
-];
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <FlatList
-          data={COLORS}
-          keyExtractor={(item, index) => index}
-          renderItem={(data) => (
-            <ColorBox
-              hexCode={data.item.hexCode}
-              colorName={data.item.colorName}
-            />
-          )}
-          ListHeaderComponent={<Text style={styles.headling}>Solarized</Text>}
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen
+          name="ColorPalette"
+          component={ColorPalette}
+          options={({ route }) => ({ title: route.params.paletteName })}
         />
-      </View>
-    </SafeAreaView>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-  },
-  headling: {
-    fontWeight: "bold",
-  },
-});
