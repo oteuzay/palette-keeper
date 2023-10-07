dd2dbc742e39ff3e73eecf528075545cbedcdeae
oteuzay <138787819+oteuzay@users.noreply.github.com>
2023-10-07 19:55:53 +0300

    feat: :sparkles: create screen of `ColorPaletteModal`

---
+++ mobile-client/screens/ColorPaletteModal.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const ColorPaletteModal = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Color Palette Modal</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+});
+
+export default ColorPaletteModal;
